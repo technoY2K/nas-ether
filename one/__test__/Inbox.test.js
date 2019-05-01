67b346eb3dce3b5dd113821907f2f7521463ccee
jhamPac <cloud@kevia.me>
2019-04-30 19:48:02 -0700

    ganache provider spins up local network

--- one/__test__/Inbox.test.js
+++ one/__test__/Inbox.test.js
@@ -2,8 +2,11 @@ const ganache = require('ganache-cli')
 const Web3    = require('web3')
 const w3      = new Web3(ganache.provider())
 
-beforeEach(() => {
-  
+beforeEach(async () => {
+  const accounts = await w3.eth.getAccounts().catch(error => {
+    throw new Error(error);
+  })
+  console.log(accounts)
 })
 
 describe('Inbox.sol test', () => {
