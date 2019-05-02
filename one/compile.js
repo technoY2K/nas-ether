01dac7f31a126d28e604042ab6852e67824b297e
jhamPac <cloud@kevia.me>
2019-05-01 21:14:21 -0700

    re order imports

--- one/compile.js
+++ one/compile.js
@@ -1,5 +1,5 @@
-const path = require('path')
 const fs   = require('fs')
+const path = require('path')
 const solc = require('solc')
 
 const inboxPath = path.resolve(__dirname, './contracts/Inbox.sol')
