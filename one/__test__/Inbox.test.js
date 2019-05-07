8f8ec591847065ef7bdf47f444a64d62738ace41
jhamPac <cloud@kevia.me>
2019-05-06 21:24:55 -0700

    contract deploy is giving an async error

--- one/__test__/Inbox.test.js
+++ one/__test__/Inbox.test.js
@@ -4,12 +4,19 @@ const w3      = new Web3(ganache.provider())
 const { abi, evm: {bytecode} } = require('../compile')
 
 let accounts;
+let inbox;
+
 beforeEach(async () => {
   accounts = await w3.eth.getAccounts()
-})
+  inbox    = await new w3.eth.Contract(abi)
+    .deploy({data: `0x${bytecode.object}`, arguments: ['The first ever!']})
+    .send({from: accounts[0], gas: '1000000'})
+    .then(r => console.log(r))
+    .catch(e => console.log(e))
+}, 50000)
 
 describe('Inbox.sol test', () => {
   test('accounts variable should be an array', () => {
-    expect(Array.isArray(accounts)).toBe(true)
+    console.log(inbox)
   })
 })
