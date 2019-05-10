9c08ecf1a117e7d49bf774e12a551abd19893ae2
jhamPac <cloud@kevia.me>
2019-05-09 18:24:21 -0700

    save point still figuring out deploying contracts

--- one/__test__/Inbox.test.js
+++ one/__test__/Inbox.test.js
@@ -8,12 +8,8 @@ let inbox;
 
 beforeEach(async () => {
   accounts = await w3.eth.getAccounts()
-  inbox    = await new w3.eth.Contract(abi)
-    .deploy({data: `0x${bytecode.object}`, arguments: ['The first ever!']})
-    .send({from: accounts[0], gas: '1000000'})
-    .then(r => console.log(r))
-    .catch(e => console.log(e))
-}, 50000)
+  inbox    = await new w3.eth.Contract(abi, accounts[0])
+})
 
 describe('Inbox.sol test', () => {
   test('accounts variable should be an array', () => {
