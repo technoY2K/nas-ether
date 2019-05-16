fb445da2372a75ae2a0064e61bd3f385c08da29d
jhamPac <cloud@kevia.me>
2019-05-15 21:06:01 -0700

    save point for deploy script

--- one/deploy.js
+++ one/deploy.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const HDWalletProvider = require('truffle-hdwallet-provider')
 const Web3 = require('web3')
 const { abi, evm: {bytecode} } = require('./compile')
 
 const provider = new HDWalletProvider(
-  '',
-  ''
+  process.env.MNEMONIC,
+  process.env.TEST_NETWORK
 )
+
+const web3 = new Web3(provider)
