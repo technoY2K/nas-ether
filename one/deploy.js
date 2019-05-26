cc1881c3f09d6aa250babdc4f1068731b0bb146d
jhamPac <cloud@kevia.me>
2019-05-26 09:41:34 -0700

    removed console.log

--- one/deploy.js
+++ one/deploy.js
@@ -12,7 +12,6 @@ const web3 = new Web3(provider)
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts()
-  console.log(accounts)
 
   const result = await new web3.eth.Contract(abi)
    .deploy({data: '0x' + bytecode.object, arguments: ['Sandstorm']})
